0a4a9ff6b5bd0f4b4b66b30af341bb36f2971631
Adam Syarif <adamsyarif@users.noreply.github.com>
2020-11-20 19:03:08 +0800

    Update blogger.js

--- blogger.js
+++ blogger.js
@@ -1,28 +1,32 @@
 function randomPost(data){
   const a = [];
-  let r, x;
   const d = data.feed.entry;
-  while(a.length < 5){
+  const s = (d.length >= 5)? 5 : d.length;
+  let r, f;
+  while(a.length < s){
     r = Math.floor(Math.random()*d.length);
-    a.push(d[r]);
+    if(a.length == 0) a.push(d[r]);
+    else {
+      a.forEach((v)=>{
+        f = (v.id.$t == d[r].id.$t)? true : false;
+      });
+      if(!f) a.push(d[r]);
+    }
   }
   let l = '';
   a.forEach((v)=>{
-    l += '<table class="w3-table">'+
+    l += '<table style="width:100%">'+
           '<tr>'+
             '<td>'+
-              '<div class="thumbnail w3-card-2">'+
-                '<a href="'+ v.link[2].href +'">'+
-                  '<img src="'+ v.media$thumbnail.url +'"/>'+
-                '</a>'+
+              '<div class="thumbnail w3-card-2 w3-margin-right">'+
+                '<img src="'+ v.media$thumbnail.url +'"/>'+
               '</div>'+
             '</td>'+
             '<td style="width:100%">'+
-              '<b class="w3-text-dark-gray">'+ v.title.$t +'</b>'+
-              '<div style="text-align:justify">'+
-                '<a href="'+ v.link[2].href +'">'+
-                  '<span class="w3-small">'+ (v.summary.$t.slice(0, 100) +'..') +'</span>'+
-                '</a>'+
+              '<b class="w3-large w3-text-dark-gray">'+ v.title.$t +'</b>'+
+              '<div class="w3-small" style="text-align:justify">'+ (v.summary.$t.slice(0, 100) +'..') +'</div>'+
+              '<div class="w3-right-align">'+
+                '<a class="w3-button w3-border w3-small w3-round-large" href="'+ v.link[2].href +'">Baca selengkapnya</a>'+
               '</div>'+
             '</td>'+
           '</tr>'+
