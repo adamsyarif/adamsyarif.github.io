be3838a595cc6b2f991ee73132fc1c5c264e3ea5
Adam Syarif <adamsyarif@users.noreply.github.com>
2020-11-20 18:05:34 +0800

    Update blogger.js

--- blogger.js
+++ blogger.js
@@ -1,3 +1,39 @@
 function randomPost(data){
-  console.log(data);
+  const a = [];
+  let r, x;
+  const d = data.feed.entry;
+  while(a.length < 5){
+    r = Math.floor(Math.random()*d.length);
+    if(a.length == 0) a.push(d[r]);
+    else {
+      x = false;
+      a.forEach((v)=>{
+        if(v.id.$t == d[r].id.$t) x = true;
+      });
+      if(!x) a.push(d[r]);
+    }
+  }
+  let l = '';
+  a.forEach((v)=>{
+    '<table class="w3-table">'+
+      '<tr>'+
+        '<td>'+
+          '<div class="thumbnail w3-card-2">'+
+            '<a href="'+ v.link[2].href +'">'+
+              '<img src="'+ v.media$thumbnail.url +'"/>'+
+            '</a>'+
+          '</div>'+
+        '</td>'+
+        '<td style="width:100%">'+
+          '<b class="w3-text-dark-gray">'+ v.title.$t +'</b>'+
+          '<div style="text-align:justify">'+
+            '<a href="'+ v.link[2].href +'">'+
+              '<span class="w3-small">'+ (v.summary.$t.slice(0, 100) +'..') +'</span>'+
+            '</a>'+
+          '</div>'+
+        '</td>'+
+      '</tr>'+
+    '</table>';
+  });
+  $('.random-post').html(l);
 }
