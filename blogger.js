d915d0b68275c790a7bc20335b86aef39ef31041
Adam Syarif <adamsyarif@users.noreply.github.com>
2020-11-20 18:28:41 +0800

    Update blogger.js

--- blogger.js
+++ blogger.js
@@ -8,25 +8,25 @@ function randomPost(data){
   }
   let l = '';
   a.forEach((v)=>{
-    '<table class="w3-table">'+
-      '<tr>'+
-        '<td>'+
-          '<div class="thumbnail w3-card-2">'+
-            '<a href="'+ v.link[2].href +'">'+
-              '<img src="'+ v.media$thumbnail.url +'"/>'+
-            '</a>'+
-          '</div>'+
-        '</td>'+
-        '<td style="width:100%">'+
-          '<b class="w3-text-dark-gray">'+ v.title.$t +'</b>'+
-          '<div style="text-align:justify">'+
-            '<a href="'+ v.link[2].href +'">'+
-              '<span class="w3-small">'+ (v.summary.$t.slice(0, 100) +'..') +'</span>'+
-            '</a>'+
-          '</div>'+
-        '</td>'+
-      '</tr>'+
-    '</table>';
+    l += '<table class="w3-table">'+
+          '<tr>'+
+            '<td>'+
+              '<div class="thumbnail w3-card-2">'+
+                '<a href="'+ v.link[2].href +'">'+
+                  '<img src="'+ v.media$thumbnail.url +'"/>'+
+                '</a>'+
+              '</div>'+
+            '</td>'+
+            '<td style="width:100%">'+
+              '<b class="w3-text-dark-gray">'+ v.title.$t +'</b>'+
+              '<div style="text-align:justify">'+
+                '<a href="'+ v.link[2].href +'">'+
+                  '<span class="w3-small">'+ (v.summary.$t.slice(0, 100) +'..') +'</span>'+
+                '</a>'+
+              '</div>'+
+            '</td>'+
+          '</tr>'+
+        '</table>';
   });
   $('.random-post').html(l);
 }
