6f6932df8a18c17f79cd51c8af98dd9f2f18064c
Adam Syarif <adamsyarif@users.noreply.github.com>
2020-11-21 15:17:19 +0800

    Update search-new.js

--- blogger/search-new.js
+++ blogger/search-new.js
@@ -1,6 +1,7 @@
 (()=>{
   _req(_blogUrl +'/feeds/posts/default?alt=json&max-results=5', (j)=>{
     _loader(true);
+    _category(j.feed.category);
     const p = [];
     const e = j.feed.entry;
     if(j.feed.entry){
