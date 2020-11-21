60599c367f97308dea7a475199dd5ee80c4466c4
Adam Syarif <adamsyarif@users.noreply.github.com>
2020-11-21 19:34:14 +0800

    Add files via upload

---
+++ blogger/search-label.js
@@ -0,0 +1,17 @@
+(()=>{
+  const p = new URL(window.location.href).pathname;
+  const c = p.slice((p.lastIndexOf('/')+1), p.length);
+  _req(_feedUrl +'/-/'+ c +'?alt=json&max-results=1000', (j)=>{
+    _loader(true);
+    const p = [];
+    if(j.feed.entry){
+      const e = j.feed.entry;
+      const x = (e.length >= 7)? 7 : e.length;
+      for(let i = 0; i < x; i++){
+        p.push(e[i]);
+      }
+    }
+    $('#search-result').html(_postList(p));
+    _loader(false);
+  });
+})();
