75421277a4aebdbf0bd9ae07d93766eb73987a22
Adam Syarif <adamsyarif@users.noreply.github.com>
2020-11-21 13:19:40 +0800

    Update and rename search-page.js to search-query.js

--- blogger/search-query.js
+++ blogger/search-query.js
@@ -2,9 +2,7 @@
   const u = new URL(window.location.href);
   const c = u.searchParams.get('c');
   const q = u.searchParams.get('q');
-  let l = _blogUrl +'/feeds/posts/default'+ (c ? ('/-/'+ c +'?') : '?') + (q ? ('q='+ q +'&') : '') +'alt=json&max-results=1000';
-  console.log(l);
-  _req(l, (j)=>{
+  _req(_blogUrl +'/feeds/posts/default'+ (c ? ('/-/'+ c +'?') : '?') + (q ? ('q='+ q +'&') : '') +'alt=json&max-results=1000', (j)=>{
     _loader(true);
     const p = [];
     if(j.feed.entry){
