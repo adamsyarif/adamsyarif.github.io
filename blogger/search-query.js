fe76030fdfe702cdd63b507942c2998e28664f0d
Adam Syarif <adamsyarif@users.noreply.github.com>
2020-11-21 12:21:47 +0800

    Update search-query.js

--- blogger/search-query.js
+++ blogger/search-query.js
@@ -1,3 +1,16 @@
 (()=>{
-  $('#demox').html(window.location.href);
+  const u = new URL(window.location.href);
+  const c = u.searchParams.get('c');
+  const q = u.searchParams.get('q');
+  _req(_blogUrl +'/feeds/posts/default'+ (c ? ('/-/'+ c) : '') +'?q='+ q +'&alt=json&max-results=1000', (j)=>{
+    _loader(true);
+    const p = [];
+    const e = j.feed.entry;
+    const x = (e.length >= 7)? 7 : e.length;
+    for(let i = 0; i < x; i++){
+      p.push(e[i]);
+    }
+    $('#search-result').html(_postList(p));
+    _loader(false);
+  });
 })();
