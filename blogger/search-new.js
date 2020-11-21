a8b1dd008ece1c16a044abbd1c950b2f45df2a36
Adam Syarif <adamsyarif@users.noreply.github.com>
2020-11-21 16:03:13 +0800

    Update search-new.js

--- blogger/search-new.js
+++ blogger/search-new.js
@@ -3,8 +3,8 @@
     _loader(true);
     _category(j.feed.category);
     const p = [];
-    const e = j.feed.entry;
     if(j.feed.entry){
+      const e = j.feed.entry;
       const x = (e.length >= 5)? 5 : e.length;
       for(let i = 0; i < x; i++){
         p.push(e[i]);
