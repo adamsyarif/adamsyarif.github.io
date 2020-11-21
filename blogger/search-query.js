3cdd8f7cf7a5819ed101a213b6a8719330a93d1d
Adam Syarif <adamsyarif@users.noreply.github.com>
2020-11-21 12:48:48 +0800

    Update search-query.js

--- blogger/search-query.js
+++ blogger/search-query.js
@@ -2,7 +2,9 @@
   const u = new URL(window.location.href);
   const c = u.searchParams.get('c');
   const q = u.searchParams.get('q');
-  _req(_blogUrl +'/feeds/posts/default'+ (c ? ('/-/'+ c) : '') +'?q='+ q +'&alt=json&max-results=1000', (j)=>{
+  let l = _blogUrl +'/feeds/posts/default'+ (c ? ('/-/'+ c) : '') +'?q='+ q +'&alt=json&max-results=1000';
+	console.log(l);
+	_req(l, (j)=>{
     _loader(true);
     const p = [];
     if(j.feed.entry){
