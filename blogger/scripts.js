9cd8226d31ba8fa17bb864b35e10a6d5b1c9b81b
Adam Syarif <adamsyarif@users.noreply.github.com>
2021-01-09 00:09:40 +0800

    Update scripts.js

--- blogger/scripts.js
+++ blogger/scripts.js
@@ -24,6 +24,9 @@ let _TOAST, _n = 0;
   });
   $('#post-body').on('contextmenu copy cut', _preventDefault);
   _showTitle(_n);
+  setInterval(()=>{
+    $('.text.pointer').toggleClass('w3-text-white w3-text-gray');
+  }, 900);
 })();
 
 function _preventDefault(e){
