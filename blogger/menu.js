1d16af8b18ebc4db1e8532b9a640cb9c5f60daf4
Adam Syarif <adamsyarif@users.noreply.github.com>
2021-02-08 23:57:20 +0800

    Update menu.js

--- blogger/menu.js
+++ blogger/menu.js
@@ -1,148 +1,183 @@
 const _m1 = {
   html: [
     {
-      id: 4399303728456275015,
+      id: 1,
       name: 'Apa itu HTML ?',
       link: '/2020/12/apa-itu-html.html'
     },
     {
-      id: 7888608571628476824,
-      name: 'HTML Attributes',
-      link: '/2021/01/html-attributes.html'
+      id: 1,
+      name: 'HTML Attribute',
+      link: '/2021/02/html-attribute.html'
     },
     {
-      id: 4333501711927359046,
+      id: 1,
       name: 'HTML Head and Body',
-      link: '/2021/01/html-head-and-body.html'
+      link: '/2021/02/html-head-and-body.html'
     },
     {
-      id: 659855880949546929,
+      id: 1,
       name: 'HTML Layout',
-      link: '/2021/01/html-layout.html'
+      link: '/2021/02/html-layout.html'
     },
     {
-      id: 2643188050306825353,
+      id: 1,
       name: 'HTML Display',
-      link: '/2021/01/html-display.html'
+      link: '/2021/02/html-display.html'
+    },
+    {
+      id: 1,
+      name: 'HTML Heading',
+      link: '/2021/02/html-heading.html'
+    },
+    {
+      id: 1,
+      name: 'HTML Text formatting',
+      link: '/2021/02/html-text-formatting.html'
+    },
+    {
+      id: 1,
+      name: 'HTML List',
+      link: '/2021/02/html-list.html'
+    },
+    {
+      id: 1
+      name: 'HTML Table',
+      link: '/2021/02/html-table.html'
+    },
+    {
+      id: 1
+      name: 'HTML Input',
+      link: '/2021/02/html-input.html'
+    },
+    {
+      id: 1
+      name: 'HTML Form',
+      link: '/2021/02/html-form.html'
+    },
+    {
+      id: 1
+      name: 'HTML Image',
+      link: '/2021/02/html-image.html'
     }
   ],
   css: [
     {
-      id: 2972016429648288247,
+      id: 1,
       name: 'Apa itu CSS ?',
       link: '/2020/12/apa-itu-css.html'
     },
     {
-      id: 1491101387658211260,
-      name: 'CSS Selectors',
-      link: '/2021/01/css-selectors.html'
+      id: 1,
+      name: 'CSS Selector',
+      link: '/2021/02/css-selector.html'
     },
     {
-      id: 8646267057938881343,
-      name: 'CSS Units',
-      link: '/2021/01/css-units.html'
+      id: 1,
+      name: 'CSS Unit',
+      link: '/2021/02/css-unit.html'
     },
     {
-      id: 3882183698891192549,
-      name: 'CSS Layout',
-      link: '/2021/01/css-layout.html'
-    },
-    {
-      id: 892818747925627275,
+      id: 1,
       name: 'CSS Display',
-      link: '/2021/01/css-display.html'
+      link: '/2021/02/css-display.html'
     },
     {
-      id: 6298600439964084840,
+      id: 1,
       name: 'CSS Position',
-      link: '/2021/01/css-position.html'
+      link: '/2021/02/css-position.html'
     },
     {
-      id: 328380100033177187,
+      id: 1,
+      name: 'CSS Margin and Padding',
+      link: '/2021/02/css-margin-and-padding.html'
+    },
+    {
+      id: 1,
       name: 'CSS Width and Height',
-      link: '/2021/01/css-width-and-height.html'
+      link: '/2021/02/css-width-and-height.html'
     },
     {
-      id: 1374602737535412614,
-      name: 'CSS Text',
-      link: '/2021/01/css-text.html'
+      id: 1,
+      name: 'CSS Alignment',
+      link: '/2021/02/css-alignment.html'
     },
     {
-      id: 1008812938349504284,
-      name: 'CSS Colors',
-      link: '/2021/01/css-colors.html'
+      id: 1,
+      name: 'CSS Font and Text',
+      link: '/2021/02/css-font-and-text.html'
     },
     {
-      id: 6181110356406152695,
-      name: 'CSS Effect',
-      link: '/2021/01/css-effect.html'
+      id: 1,
+      name: 'CSS Image',
+      link: '/2021/02/css-image.html'
     },
     {
-      id: 8945022256510799130,
-      name: 'CSS Animation',
-      link: '/2021/01/css-animation.html'
+      id: 1,
+      name: 'CSS Color',
+      link: '/2021/02/css-color.html'
     }
   ],
   javascript: [
     {
-      id: 5921124459974078627,
+      id: 1,
       name: 'Apa itu JavaScript ?',
       link: '/2020/12/apa-itu-javascript.html'
     },
     {
-      id: 5654482686799684914,
+      id: 1,
       name: 'JavaScript Variable',
-      link: '/2021/01/javascript-variable.html'
+      link: '/2021/02/javascript-variable.html'
     },
     {
-      id: 4090851026629334638,
+      id: 1,
       name: 'JavaScript Data type',
-      link: '/2021/01/javascript-data-type.html'
+      link: '/2021/02/javascript-data-type.html'
     },
     {
-      id: 3104696424489244361,
+      id: 1,
       name: 'JavaScript Operator',
-      link: '/2021/01/javascript-operator.html'
+      link: '/2021/02/javascript-operator.html'
     },
     {
-      id: 5612245712293651737,
+      id: 1,
       name: 'JavaScript Conditional statement',
-      link: '/2021/01/javascript-conditional-statement.html'
+      link: '/2021/02/javascript-conditional-statement.html'
     },
     {
-      id: 5474250890539883776,
+      id: 1,
       name: 'JavaScript Function',
-      link: '/2021/01/javascript-function.html'
+      link: '/2021/02/javascript-function.html'
     },
     {
-      id: 2265176596142947677,
+      id: 1,
       name: 'JavaScript DOM',
       link: '/2021/02/javascript-dom.html'
     },
     {
-      id: 4023070051762934749,
+      id: 1,
       name: 'JavaScript Event listener',
-      link: '/2021/01/javascript-event-listener.html'
+      link: '/2021/02/javascript-event-listener.html'
     },
     {
-      id: 2265176596142947677,
+      id: 1,
       name: 'JavaScript Loop',
       link: '/2021/02/javascript-loop.html'
     },
     {
-      id: 8724253155128548012,
+      id: 1,
       name: 'JavaScript Math',
       link: '/2021/02/javascript-math.html'
     },
     {
-      id: 7783484893767550398,
+      id: 1,
       name: 'JavaScript Date',
       link: '/2021/02/javascript-date.html'
     },
     {
-      id: 1206236644980774137,
-      name: 'JavaScript Try Catch',
-      link: '/2021/02/javascript-try-catch.html'
+      id: 1,
+      name: 'JavaScript Error handling',
+      link: '/2021/02/javascript-error-handling.html'
     }
   ]
 };
@@ -150,58 +185,108 @@ const _m1 = {
 const _m2 = {
   html: [
     {
-      id: 0,
-      name: '-',
-      link: '/'
+      id: 1
+      name: 'HTML Responsive',
+      link: '/2021/02/html-responsive.html'
+    },
+    {
+      id: 1
+      name: 'HTML Iframe',
+      link: '/2021/02/html-iframe.html'
+    },
+    {
+      id: 1
+      name: 'HTML Media player',
+      link: '/2021/02/html-media-player.html'
+    },
+    {
+      id: 1
+      name: 'HTML Entities',
+      link: '/2021/02/html-entities.html'
     }
   ],
   css: [
     {
-      id: 0,
-      name: '-',
-      link: '/'
+      id: 1,
+      name: 'CSS Effect',
+      link: '/2021/02/css-effect.html'
+    },
+    {
+      id: 1,
+      name: 'CSS Flexbox',
+      link: '/2021/02/css-flexbox.html'
+    },
+    {
+      id: 1,
+      name: 'CSS Grid layout',
+      link: '/2021/02/css-grid-layout.html'
+    },
+    {
+      id: 1,
+      name: 'CSS Icon',
+      link: '/2021/02/css-icon.html'
+    },
+    {
+      id: 1,
+      name: 'CSS Animation',
+      link: '/2021/02/css-animation.html'
     }
   ],
   javascript: [
     {
-      id: 5921124459974078627,
+      id: 1,
+      name: 'JavaScript BOM',
+      link: '/2021/02/javascript-bom.html'
+    },
+    {
+      id: 1,
+      name: 'JavaScript JSON',
+      link: '/2021/02/javascript-json.html'
+    },
+    {
+      id: 1,
       name: 'JavaScript Object destructuring',
-      link: '/2021/01/javascript-object-destructuring.html'
+      link: '/2021/02/javascript-object-destructuring.html'
     },
     {
-      id: 5921124459974078627,
+      id: 1,
       name: 'JavaScript Array function',
-      link: '/2021/01/javascript-array-function.html'
+      link: '/2021/02/javascript-array-function.html'
     },
     {
-      id: 5921124459974078627,
+      id: 1,
       name: 'JavaScript Promise',
-      link: '/2021/01/javascript-promise.html'
+      link: '/2021/02/javascript-promise.html'
     },
     {
-      id: 1989043964910531869,
+      id: 1,
       name: 'JavaScript Async Await',
-      link: '/2021/01/javascript-async-await.html'
+      link: '/2021/02/javascript-async-await.html'
     },
     {
-      id: 8718410627563179637,
+      id: 1,
       name: 'JavaScript this Keyword',
-      link: '/2021/01/javascript-this-keyword.html'
+      link: '/2021/02/javascript-this-keyword.html'
     },
     {
-      id: 8718410627563179637,
-      name: 'JavaScript Arrow function',
-      link: '/2021/01/javascript-arrow-function.html'
-    },
-    {
-      id: 530437586833937029,
+      id: 1,
       name: 'JavaScript Class',
-      link: '/2021/01/javascript-class.html'
+      link: '/2021/02/javascript-class.html'
     },
     {
-      id: 6037888841435560037,
+      id: 1,
+      name: 'JavaScript AJAX',
+      link: '/2021/02/javascript-ajax.html'
+    },
+    {
+      id: 1,
+      name: 'JavaScript Web API',
+      link: '/2021/02/javascript-web-api.html'
+    },
+    {
+      id: 1,
       name: 'JavaScript RegExp',
-      link: '/2021/01/javascript-regexp.html'
+      link: '/2021/02/javascript-regexp.html'
     }
   ]
 };
