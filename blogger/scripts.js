db1244f47a09de4969bfc05187a1427656a1e2a9
Adam Syarif <adamsyarif@users.noreply.github.com>
2021-02-17 14:45:10 +0800

    Update scripts.js

--- blogger/scripts.js
+++ blogger/scripts.js
@@ -210,7 +210,7 @@ const _scrollTop = ()=> $('#inner-wrapper').animate({scrollTop:0}, 800);
     $('#searchbar select').html(l);
   });
   if(typeof(Worker) != 'undefined'){
-    const w = new Worker('https://blogger.wbp.workers.dev');
+    const w = new Worker('http://adamsyarif.github.io/blogger/workers.js');
     w.onmessage = (e)=>{
       const t = $('.titles');
       if(e.data.text) t.find('b').text(e.data.text);
