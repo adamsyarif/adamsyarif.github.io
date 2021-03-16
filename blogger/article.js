fd6857686143d28b4c5bfac6db5eb47e361cec55
Adam Syarif <adamsyarif@users.noreply.github.com>
2021-03-16 13:36:32 +0800

    Update article.js

--- blogger/article.js
+++ blogger/article.js
@@ -99,4 +99,24 @@ FEED.newer = ()=>{
     n += '<button onclick="_jumpTo('+ (Math.round($(this).offset().top)-150) +')" class="w3-bar-item w3-button">'+ $(this).text() +'</button>';
   });
   $('#title-nav nav').html(n);
+  const p = $('.preview');
+  p.find('button').off('click');
+  p.find('button.active').click(function(){
+    const a = $(this).parent().parent();
+    const b = a.find('button');
+    const d = a.find('div');
+    const i = b.index(this);
+    b.removeClass('w3-border-blue');
+    b.eq(i).addClass('w3-border-blue');
+    d.hide();
+    d.eq(i).show();
+  });
+  p.find('iframe').attr('srcdoc', function(){
+    const a = $(this).parent().parent();
+    if(a.find('button').eq(0).hasClass('active')){
+      const c = a.find('code');
+      return (c.eq(0).text() +'<style>html{box-sizing:border-box}*,*:before,*:after{box-sizing:inherit}body{margin:0;padding:0}'+ c.eq(1).text() +'</style><script>'+ c.eq(2).text() +'</script>');
+    }
+    else $(this).remove();
+  });
 })();
