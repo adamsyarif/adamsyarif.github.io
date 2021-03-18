4ecc8fea3a20ced71ad62365b8e895b061d9b92d
Adam Syarif <adamsyarif@users.noreply.github.com>
2021-03-18 13:37:01 +0800

    Update scripts.js

--- blogger/scripts.js
+++ blogger/scripts.js
@@ -1,168 +1,165 @@
-const ENV = {};
-ENV.repoUrl = 'https://adamsyarif.github.io/blogger/';
-ENV.blogUrl = 'https://web-belajar-pemrograman.blogspot.com/';
-
-const APP = {
-  REQ: [],
-  req: (u, c)=>{
-    $.ajax({
-      method: 'GET',
-      url: u,
-      beforeSend: ()=> APP.loader(true),
-      complete: ()=> APP.loader(false)
-    }).done(r => c(r)).fail((x, s, e)=>{
-      e ? (
-        APP.toast('Error, please check console for details'),
-        console.log(e)
-      ) : APP.toast(s);
-    });
-  },
-  loader: s =>{
-    const l = $('#loader');
-    if(s){
-      if(APP.REQ.length == 0) l.show();
-      APP.REQ.push(1);
-    } else {
-      if(APP.REQ.length > 0) APP.REQ.pop();
-      if(APP.REQ.length == 0) l.hide();
-    }
-  },
-  TOAST: null,
-  toast: m =>{
-    if(APP.TOAST) clearTimeout(APP.TOAST);
-    const t = $('#toast');
-    t.show().find('div').text(m);
-    APP.TOAST = setTimeout(()=>{
-      APP.TOAST = null;
-      t.hide();
-    }, 3000);
-  },
-  worker: (u, c)=>{
-    APP.req(u, r =>{
-      const b = new Blob([r], {type:'application/javascript'});
-      c(new Worker(URL.createObjectURL(b)));
-    });
-  },
-  copy: e =>{
-    $(e).select();
-    document.execCommand('copy');
-    APP.toast('Teks telah disalin');
-  },
-  preventDefault: e =>{
-    e.preventDefault();
-    return false;
-  }
-};
-
-const FEED = {
-  u1: ENV.blogUrl +'search',
-  u2: ENV.blogUrl +'feeds/posts/default',
-  u3: 'alt=json&max-results=',
-  search: ()=>{
-    const s = $('#searchbar');
-    (+$('[name=searchbar]:checked').val() == 1)? (()=>{
-      const q = s.find(':text').val().trim();
-      q ? location.assign(encodeURI(FEED.u1 +'?q='+ q)) : APP.toast('Kata kunci tidak boleh kosong');
-    })() : location.assign(encodeURI(FEED.u1 +'/label/'+ s.find('select').val()));
-  }
+const ENV = {
+  devMode: true,
+  repoUrl: 'https://adamsyarif.github.io/blogger/',
+  blogUrl: 'https://web-belajar-pemrograman.blogspot.com/'
 };
 
 const RUN = {
-  displayMenu: ()=>{
-    MENU.page_id = +$('#page-id').val();
-    MENU.article_id = +$('#article-id').val();
-    MENU.article = (index, data)=>{
-      const x = (i, a)=> $('.article-nav').eq(i).show().find('a').attr('href', a.link).text(a.title);
-      const p = data[index-1];
-      const n = data[index+1];
-      if(p && p.link != '#') x(0, p);
-      if(n && n.link != '#') x(1, n);
-    };
-    MENU.list = function(data){
-      let c, l = '';
-      data.forEach((i, x, d)=>{
-        c = '';
-        if((i.id == this.page_id) || (i.id == this.article_id)){
-          c = 'w3-light-gray w3-rightbar';
-          (i.id == this.page_id)? this.page(x, d) : this.article(x, d);
-        }
-        l += '<a '+ ((i.link != '#')? ('href="'+ i.link +'"') : '') +' class="'+ c +' w3-bar-item w3-button w3-hover-light-gray"><i class="far fa-file-alt w3-margin-right"></i>'+ i.title + ((i.link != '#')? '' : ' <sup class="w3-text-red">(<i>draft</i>)</sup>') +'</a>';
-      });
-      return l;
-    };
-    MENU.docs = list => '<div class="w3-margin-left" style="display:none">'+ list +'</div>';
-    MENU.folder = name => '<button class="w3-bar-item w3-button w3-hover-light-gray" onclick="RUN.toggleFolder(this)"><i class="fas fa-folder w3-text-yellow w3-margin-right"></i>'+ name +'</button>';
-    MENU.folders = function(folders){
-      let f = '';
-      folders.forEach(folder =>{
-        f += this.folder(folder.title);
-        f += this.docs(this.list(folder.articles));
-      });
-      return f;
-    };
-    MENU.navigation = function(){
-      let m2, m1 = '';
-      Object.keys(this).forEach(key =>{
-        if(this[key].title){
-          m1 += this.folder(this[key].title);
-          m2 = (this[key].folders)? this.folders(this[key].folders) : this.list(this[key].pages);
-          m1 += this.docs(m2);
-        }
-      });
-      return m1;
-    };
-    $('#menubar').find('nav').html(MENU.navigation());
+  sectionFeed: ()=>{
+    const t = +$('#section-type').val();
+    if(t){
+      const n = $('#section-name');
+      if(t == 1){
+        n.text('Artikel terkait');
+        FEED.related();
+      } else {
+        n.text('Artikel terbaru');
+        FEED.newer();
+      }
+    }
   },
-  toggleFolder: e =>{
-    $(e).find('i').toggleClass('fa-folder fa-folder-open');
-    $(e).next().slideToggle();
-  }
-};
-
-$(document).ready(()=>{
-  /* Start workers */
-
-  if(typeof(Worker) != 'undefined'){
-    APP.worker(ENV.repoUrl +'workers.js', w =>{
+  worker: ()=>{
+    const u = (ENV.devMode ? '/scripts/' : ENV.repoUrl) +'workers.js';
+    APP.worker(u, w =>{
       w.onmessage = e =>{
         const t = $('.titles');
         if(e.data.text) t.find('b').text(e.data.text);
         if(e.data.pointer) t.find('span').toggleClass('w3-text-white w3-text-gray');
       };
     });
-  }
-
-  /* DOM content */
-
-  RUN.displayMenu();
-  $('.what-time-is').text(()=>{
-    const h = new Date().getHours();
-    return (h >= 3 && h <= 10)? 'pagi' : (h >= 11 && h <= 14)? 'siang' : (h >= 15 && h <= 17)? 'sore' : 'malam';
-  });
-  $('.copyright-year').text(new Date().getFullYear());
-  $('body').fadeIn(1000);
-  /*
-  setTimeout(()=>{
-    $('#cover').fadeOut(1000, function(){
-      $(this).remove();
+  },
+  displayMenu: ()=>{
+    const u = (ENV.devMode ? '/data/' : ENV.repoUrl) +'menu.json';
+    APP.req(u, r =>{
+      r.page_id = +$('#page-id').val();
+      r.article_id = +$('#article-id').val();
+      r.article = (index, data)=>{
+        const x = (i, a)=> $('.article-nav').eq(i).show().find('a').attr('href', a.link).text(a.title);
+        const p = data[index-1];
+        const n = data[index+1];
+        if(p && p.link != '#') x(0, p);
+        if(n && n.link != '#') x(1, n);
+      };
+      r.list = function(data){
+        let c, l = '';
+        data.forEach((i, x, d)=>{
+          c = '';
+          if((i.id == this.page_id) || (i.id == this.article_id)){
+            c = 'w3-light-gray w3-rightbar';
+            (i.id == this.page_id)? this.page(x, d) : this.article(x, d);
+          }
+          l += '<a '+ ((i.link != '#')? ('href="'+ i.link +'"') : '') +' class="'+ c +' w3-bar-item w3-button w3-hover-light-gray"><i class="far fa-file-alt w3-margin-right"></i>'+ i.title + ((i.link != '#')? '' : ' <sup class="w3-text-red">(<i>draft</i>)</sup>') +'</a>';
+        });
+        return l;
+      };
+      r.docs = list => '<div class="w3-margin-left" style="display:none">'+ list +'</div>';
+      r.folder = name => '<button class="w3-bar-item w3-button w3-hover-light-gray" onclick="RUN.toggleFolder(this)"><i class="fas fa-folder w3-text-yellow w3-margin-right"></i>'+ name +'</button>';
+      r.folders = function(folders){
+        let f = '';
+        folders.forEach(folder =>{
+          f += this.folder(folder.title);
+          f += this.docs(this.list(folder.articles));
+        });
+        return f;
+      };
+      r.navigation = function(){
+        let m2, m1 = '';
+        Object.keys(this).forEach(key =>{
+          if(this[key].title){
+            m1 += this.folder(this[key].title);
+            m2 = (this[key].folders)? this.folders(this[key].folders) : this.list(this[key].pages);
+            m1 += this.docs(m2);
+          }
+        });
+        return m1;
+      };
+      $('#menubar').find('nav').html(r.navigation());
     });
-  }, 100);
-  */
+  },
+  domContent: ()=>{
+    $('.what-time-is').text(()=>{
+      const h = new Date().getHours();
+      return (h >= 3 && h <= 10)? 'pagi' : (h >= 11 && h <= 14)? 'siang' : (h >= 15 && h <= 17)? 'sore' : 'malam';
+    });
+    $('.copyright-year').text(new Date().getFullYear());
+    setTimeout(()=>{
+      $('body').fadeIn(1000);
+    }, 100);
+  },
+  bindEvent: ()=>{
+    if(!ENV.devMode) $('body').on('contextmenu', APP.preventDefault);
+    $('form').submit(APP.preventDefault).trigger('reset');
+    $(':radio').change(function(){
+      $('[name='+ $(this).attr('name') +']').removeAttr('checked');
+      $(this).attr('checked', true);
+    });
+    $('#searchbar :radio').change(function(){
+      const s = $(this).parent().parent().find('.section');
+      s.hide();
+      s.eq(+$(this).val()-1).show();
+    });
+    $('[data-validation]').on('input', function(){
+      $(this).val($(this).val().replace(new RegExp('[^'+ $(this).attr('data-validation') +']', 'gim'), ''));
+    });
+  },
+  toggleFolder: f =>{
+    $(f).find('i').toggleClass('fa-folder fa-folder-open');
+    $(f).next().slideToggle();
+  }
+};
 
-  /* Event listener */
+const FEED = {
+  u1: ENV.blogUrl +'search',
+  u2: ENV.blogUrl +'feeds/posts/default',
+  u3: m => '?alt=json&max-results='+ m,
+  search: ()=>{
+    const s = $('#searchbar');
+    (+$('[name=searchbar]:checked').val() == 1)? (()=>{
+      const q = s.find(':text').val().trim();
+      q ? location.assign(encodeURI(FEED.u1 +'?q='+ q)) : APP.toast('Kata kunci tidak boleh kosong');
+    })() : location.assign(encodeURI(FEED.u1 +'/label/'+ s.find('select').val()));
+  },
+  related: ()=>{
+    const l = $('#article-label').val().split(',').filter(v => v.trim() != '');
+    const r = Math.floor(Math.random() * l.length);
+    APP.req(FEED.u2 +'/-/'+ l[r] + FEED.u3(1000), RESULT.section);
+  },
+  newer: ()=> APP.req(FEED.u2 + FEED.u3(10), RESULT.section)
+};
 
-  //$('body').on('contextmenu', APP.preventDefault);
-  $('form').submit(APP.preventDefault).trigger('reset');
-  $(':radio').change(function(){
-    $('[name='+ $(this).attr('name') +']').removeAttr('checked');
-    $(this).attr('checked', true);
-  });
-  $('#searchbar :radio').change(function(){
-    const s = $(this).parent().parent().find('.section');
-    s.hide();
-    s.eq(+$(this).val()-1).show();
-  });
-  $('[data-validation]').on('input', function(){
-    $(this).val($(this).val().replace(new RegExp('[^'+ $(this).attr('data-validation') +']', 'gim'), ''));
-  });
+const RESULT = {
+  section: d =>{
+    const e = d.feed.entry.sort(()=> Math.random()-0.5).slice(0,5);
+    $('#section-result').html(RESULT.article(e));
+  },
+  article: e =>{
+    let a = '';
+    e.forEach(d =>{
+      a += '<table style="width:100%">'+
+              '<tr>'+
+                '<td style="vertical-align:top">'+
+                  '<div class="thumbnail w3-card-2 w3-margin-right">'+
+                    '<img src="'+ d.media$thumbnail.url +'"/>'+
+                  '</div>'+
+                '</td>'+
+                '<td>'+
+                  '<b class="w3-large w3-text-dark-gray">'+ d.title.$t +'</b>'+
+                  '<div class="w3-small w3-justify">'+ d.summary.$t.slice(0,100) +'..</div>'+
+                  '<p class="w3-right-align">'+
+                    '<a class="w3-button w3-small w3-border w3-round-large" href="'+ d.link[2].href +'">Baca selengkapnya</a>'+
+                  '</p>'+
+                '</td>'+
+              '</tr>'+
+            '</table>';
+    });
+    return a;
+  }
+};
+
+$(document).ready(()=>{
+  RUN.sectionFeed();
+  RUN.worker();
+  RUN.displayMenu();
+  RUN.domContent();
+  RUN.bindEvent();
 });
