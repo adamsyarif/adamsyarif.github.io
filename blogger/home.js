ee6d26091cf899e3385c81ddd0e200f7a3ea0e84
Adam Syarif <adamsyarif@users.noreply.github.com>
2021-03-20 18:22:42 +0800

    Update home.js

--- blogger/home.js
+++ blogger/home.js
@@ -34,10 +34,10 @@ $(document).ready(()=>{
     });
     $('#testi').find('table').html('<tr>'+ (t+t) +'</tr>').css('animation', 'testi 40s linear infinite');
   });
-	$('.code').eq(0).find('code').text('body{color:red}');
-	$('.code').eq(1).find('code').text('function a(){}');
-	$('.code').eq(2).find('code').text('<div>');
-	$('pre code').each(function(){
-		hljs.highlightBlock(this);
-	});
+  $('.code').eq(0).find('code').text('body {\ncolor:red;\n}');
+  $('.code').eq(1).find('code').text('function a(){}');
+  $('.code').eq(2).find('code').text('<div>');
+  $('pre code').each(function(){
+    hljs.highlightBlock(this);
+  });
 });
