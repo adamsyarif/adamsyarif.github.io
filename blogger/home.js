a15c7c9cc4d5dbe13a36fd9a20c5ef1bd3982e8c
Adam Syarif <adamsyarif@users.noreply.github.com>
2021-03-20 18:52:53 +0800

    Update home.js

--- blogger/home.js
+++ blogger/home.js
@@ -34,9 +34,10 @@ $(document).ready(()=>{
     });
     $('#testi').find('table').html('<tr>'+ (t+t) +'</tr>').css('animation', 'testi 40s linear infinite');
   });
-  $('.code').eq(0).find('code').text('body {\ncolor:red;\n}');
-  $('.code').eq(1).find('code').text('function a(){}');
-  $('.code').eq(2).find('code').text('<div>');
+  const c = $('.code');
+  c.eq(0).find('code').text('\n body {\n   background-color: blue;\n }\n h1 {\n   text-align: center;\n }\n p {\n   font-size: 20px;\n }\n ');
+  c.eq(1).find('code').text('\n function tambah(a, b){\n   return a + b;\n }\n function kurang(a, b){\n   return a - b;\n }\n\n console.log(tambah(1, 2));\n console.log(kurang(9, 4));\n ');
+  c.eq(2).find('code').text('\n <html>\n   <head>\n     <title>Nama web</title>\n   </head>\n   <body>\n     <h1>Judul artikel</h1>\n     <p>Paragraf</p>\n   </body>\n </html>\n ');
   $('pre code').each(function(){
     hljs.highlightBlock(this);
   });
