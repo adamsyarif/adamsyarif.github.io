8a5df3586b32f35c82ddf80545eed8b6852064cf
Adam Syarif <adamsyarif@users.noreply.github.com>
2021-03-21 20:09:18 +0800

    Add files via upload

---
+++ blogger/wrapper.js
@@ -0,0 +1,13 @@
+RUN.titleNav = s =>{
+  const t = $('#title-nav');
+  if(s){
+    let x1, x2, n = '';
+    $('h4 div').each(function(){
+      x1 = $('#inner-wrapper').scrollTop();
+      x2 = $(this).offset().top;
+      n += '<button onclick="RUN.jumpTo('+ (Math.round(x1 + x2)-150) +'); RUN.titleNav(false)" class="w3-bar-item w3-button">'+ $(this).text() +'</button>';
+    });
+    t.show().find('nav').html(n);
+  }
+  else t.hide();
+};
