e694e36eef015128cb9924f0bced731179481ca2
Adam Syarif <adamsyarif@users.noreply.github.com>
2021-03-27 15:26:17 +0800

    Update wrapper.js

--- blogger/wrapper.js
+++ blogger/wrapper.js
@@ -6,7 +6,7 @@ RUN.titleNav = s =>{
       x1 = $('#body').scrollTop();
       x2 = $(this).offset().top;
 			x3 = (window.innerWidth > 600)? 150 : 75;
-      n += '<button onclick="RUN.jumpTo('+ Math.round(x1 + x2 -x3) +'); RUN.titleNav(false)" class="w3-bar-item w3-button">'+ $(this).text() +'</button>';
+      n += '<button onclick="RUN.jumpTo('+ Math.round(x1 + x2 - x3) +'); RUN.titleNav(false)" class="w3-bar-item w3-button">'+ $(this).text() +'</button>';
     });
     t.show().find('nav').html(n);
   }
