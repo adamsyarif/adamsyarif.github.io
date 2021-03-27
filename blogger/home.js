e19763ad48226caf6f97dd1377e0528a7e68c4d5
Adam Syarif <adamsyarif@users.noreply.github.com>
2021-03-27 10:01:52 +0800

    Update home.js

--- blogger/home.js
+++ blogger/home.js
@@ -20,6 +20,20 @@ const SLIDE = {
 };
 
 $(document).ready(()=>{
+	APP.data('testi', r =>{
+    let t = '';
+    r.data.sort(()=> Math.random()-0.5).slice(0,5).forEach(d =>{
+      t += '<td class="w3-container">'+
+              '<p><img class="thumbnail w3-circle w3-card-2" src="'+ d.img +'"/></p>'+
+              '<p>'+
+                '<b class="w3-large w3-text-dark-gray">'+ d.name +'</b><br/>'+
+                '<span class="w3-small w3-text-gray">'+ d.city +'</span>'+
+              '</p>'+
+              '<p><q><i>'+ d.msg +'</i></q></p>'+
+            '</td>';
+    });
+    $('#testi').find('table').html('<tr>'+ (t+t) +'</tr>').css('animation', 'testi 40s linear infinite');
+  });
 	const c = $('.code');
   c.eq(0).find('code').text('\n function tambah(a, b){\n   return a + b;\n }\n function kurang(a, b){\n   return a - b;\n }\n\n console.log(tambah(1, 2));\n console.log(kurang(9, 4));\n ');
   c.eq(1).find('code').text('\n body {\n   background-color: blue;\n }\n h1 {\n   text-align: center;\n }\n p {\n   font-size: 20px;\n }\n ');
