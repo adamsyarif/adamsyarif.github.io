9e61d74be0c3dad0108d682d9f0ba9ed60c8ed67
Adam Syarif <adamsyarif@users.noreply.github.com>
2021-03-27 08:12:38 +0800

    Update home.js

--- blogger/home.js
+++ blogger/home.js
@@ -24,7 +24,17 @@ $(document).ready(()=>{
   c.eq(0).find('code').text('\n body {\n   background-color: blue;\n }\n h1 {\n   text-align: center;\n }\n p {\n   font-size: 20px;\n }\n ');
   c.eq(1).find('code').text('\n function tambah(a, b){\n   return a + b;\n }\n function kurang(a, b){\n   return a - b;\n }\n\n console.log(tambah(1, 2));\n console.log(kurang(9, 4));\n ');
   c.eq(2).find('code').text('\n <html>\n   <head>\n     <title>Nama web</title>\n   </head>\n   <body>\n     <h1>Judul artikel</h1>\n     <p>Paragraf</p>\n   </body>\n </html>\n ');
-  $('pre code').each(function(){
+  $('.code').click(function(){
+		const r = e =>{			
+			if($(e).hasClass('code1')) $(e).removeClass('code1').addClass('code2');
+			else if($(e).hasClass('code2')) $(e).removeClass('code2').addClass('code3');
+			else $(e).removeClass('code3').addClass('code1');
+		};
+		r('.code.html');
+		r('.code.css');
+		r('.code.javascript');
+	});
+	$('pre code').each(function(){
     hljs.highlightBlock(this);
   });
 });
