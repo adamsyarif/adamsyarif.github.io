b172ff46b0271cc25c09f13698b303f8973f6cda
Adam Syarif <adamsyarif@users.noreply.github.com>
2021-04-01 22:57:59 +0800

    Update title.js

--- blogger/title.js
+++ blogger/title.js
@@ -6,13 +6,13 @@ RUN.titleNav = s =>{
       x1 = $('#body').scrollTop();
       x2 = $(this).offset().top;
       x3 = (window.innerWidth > 600)? 150 : 80;
-      n += '<button onclick="RUN.jumpTo('+ Math.round(x1 + x2 - x3) +'); RUN.titleNav(false)" class="w3-bar-item w3-button w3-hover-light-gray">'+ $(this).text() +'</button>';
+      n += '<button onclick="RUN.jumpTo('+ Math.round(x1 + x2 - x3) +'); RUN.titleNav(false)" class="w3-bar-item w3-button">'+ $(this).text() +'</button>';
     });
     t.show().find('nav').html(n);
   }
   else t.hide();
 };
 
-$(document).ready(()=>{
-  if($('h4 div').length == 0) $('.title-nav').hide();
-});
+(()=>{
+  if($('h4 div').length == 0) $('#title-nav, .title-nav').remove();
+})();
